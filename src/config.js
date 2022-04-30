af521b2f9be704e2e9322b5f0c70cf5c1aa25aed
Emilian Gospodinov <emonidi@gmail.com>
2022-04-30 11:20:23 +0200

    animation speed to x256

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 export default {
-    speeds:[1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048],
+    speeds:[1, 2, 4, 8, 16, 32, 64, 128, 256],
     mapboxAccessToken:'pk.eyJ1IjoiZW1vbmlkaSIsImEiOiJjajdqd3pvOHYwaThqMzJxbjYyam1lanI4In0.V_4P8bJqzHxM2W9APpkf1w'
 }
